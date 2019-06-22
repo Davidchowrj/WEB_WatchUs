1a15b6a079c2b5aaebd06d404b523606820cdc3a
David <davidchowrj@gmail.com>
2019-06-22 21:08:55 +0800

    change services

--- includes/header.php
+++ includes/header.php
@@ -30,14 +30,14 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="#"><img src="./images/WATCHUSLOGO2.1.PNG" alt="logo" width="100" height="auto" class="d-inline-block align-middle">WatchUS</a>
+                <a class="navbar-brand" href="./index.php"><img src="./images/WATCHUSLOGO2.1.PNG" alt="logo" width="100" height="auto" class="d-inline-block align-middle">WatchUS</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    <li class="<?php if ($page == 'home') {
+                    <li class="<?php if ($page == 'index') {
                                     echo 'active';
                                 } ?>">
                         <a class="nav-link" href="./index.php">Home </a>
--- services.php
+++ services.php
@@ -7,25 +7,20 @@ include "includes/header.php";
 ?>
 
 
-<div class="row mb-4">
-	<div class="col-md-12">
-		<div class="card" style="background-image: url(https://www.bellross.com/catalog/view/theme/bell_n_ross/images/after_sales_service.jpg)" width="1280" height="239">
-			<div class="text-white text-center d-flex align-items-center py-5 px-4 my-5">
-				<div>
-					<h1 class="card-title pt-3 mb-5 font-bold"><strong>Create a great website with MDB</strong></h1>
-					<p class="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat,
-						laboriosam, voluptatem, optio vero odio nam sit officia accusamus
-						minus error nisi architecto nulla ipsum dignissimos. Odit sed qui,
-						dolorum!.</p>
-					<a class="btn btn-outline-white"><i class="fa fa-clone left"></i> View project</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12 text-center">
+                <h1 class="font-weight-bold text-white">Services And Support </h1>
+            </div>
+        </div>
+    </div>
+</header>
 
 
-<?php
 
-include "includes/footer.php";
-?>
+            <?php
+
+            include "includes/footer.php";
+
+            ?>
