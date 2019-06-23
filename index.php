83f164bf2818ec09838ea1bbc36d26aeb40f5e06
Rranddy <randywong1029@gmail.com>
2019-06-23 22:29:28 +0800

    edited home page

--- index.php
+++ index.php
@@ -68,50 +68,42 @@ include "includes/header.php";
 
 
 <div class="container about" id="about">
-    <h1 class="text-center"> About Us</h1>
+    <h1 class="text-center"> What's Trending </h1>
     <div class="row">
         <div class="col-lg-12">
-            <p> uis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                Cras
-                mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor
-                mauris
-                condimentum nibh
-            </p>
+            
         </div>
     </div>
 
     <div class="row">
         <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <h3> Skrt </h3>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh
-                ultricies
-                vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
-                commodo cursus
-                magna.</p>
+        	<a href="https://www.gshock.com/collections/bodega">
+            <img class="rounded-circle" src="https://casiocdn.com/gshock-v2/resource/images/backgrounds/bodega-1-hd.jpg" alt="Generic placeholder image" width="140" height="140">
+            </a>
+            <h2>G-SHOCK X BODEGA</h2>
+            <h3> COLLABORATION LAUNCH PARTY </h3>
+            <p>In celebration of G-Shock's and Bodega's collaborative release, Bodega hosted a small get together in boston, bringing Bodega stocked with  
+            alphabet soup and some of their favourite DJ's.</p>
 
         </div>
         <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <h3> yeet </h3>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                Cras
-                mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor
-                mauris
-                condimentum nibh.</p>
+        	<a href="https://www.bloomberg.com/news/articles/2019-06-20/best-dive-watches">
+            <img class="rounded-circle" src="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iL0RBApSchC4/v1/800x-1.jpg" alt="Generic placeholder image" width="140" height="140">
+            </a>
+            <h2>DEEP DIVE</h2>
+            <h3> These Dive Watches Can Go Deep In the Ocean-And Help Save It, Too </h3>
+            <p>Wristwatches have been worn for diving, sailing, and other maritime activities, at least since the first waterproof watch case, the Rolex Oyster, 
+             made its debut in 1926.</p>
 
         </div>
         <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <h3> Skrt Skrt </h3>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id
-                ligula porta
-                felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
-                ut fermentum
-                massa justo sit amet risus.</p>
+        	<a href="https://www.augustman.com/sg/style/watches/watches-worn-by-influential-men-2019/">
+            <img class="rounded-circle" src="https://d1otfi4uhdq3fm.cloudfront.net/wp-content/uploads/2019/06/21151324/Virgil-Ablohs-Custom.jpg" alt="Generic placeholder image" width="140" height="140">
+            </a>
+            <h2>THE WORLD'S MOST INFLUENTIAL MEN ARE WEARING THESE WATCHES</h2>
+            <p>A gentleman's choice of timepiece says a lot about him. It is an extension of him, as it epitomises his personality, 
+            values, and style. So it comes as no surprise that many of the world's most influential men 
+            </p>
 
         </div>
     </div>
